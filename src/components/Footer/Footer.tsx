f2e86d5a6aaaabd5d536f588413e9caeb2afd3b5
Stiven Laiton <69602896+slaiton@users.noreply.github.com>
2025-10-16 10:16:05 -0500

    Se retiran datos de contacto y mapas

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -19,19 +19,6 @@ const Footer = () => {
 
 
       <div className="footer-map">
-        <iframe
-          title="Google Maps Location"
-          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7952.978046328649!2d-74.046699!3d4.684742000000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9a966cd4ac4b%3A0x99b561508fcced8f!2sCra.%2014%20%23100-19%2C%20Bogot%C3%A1%2C%20Colombia!5e0!3m2!1ses-419!2sus!4v1741886761633!5m2!1ses-419!2sus"
-          width="500"
-          height="200"
-          style={{
-            border: "0px",
-            width: "100%",
-            maxWidth: "500px",
-            minWidth: "auto",
-          }}
-          loading="lazy"
-        ></iframe>
       </div>
 
       <div className="footer-contact">
